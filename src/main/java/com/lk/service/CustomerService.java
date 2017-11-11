f34e8cda4f520379a9a4a71c2311bc06297ce076
lk21 <1622841519@qq.com>
2017-11-11 17:58:54 +0800

    添加databaseHelper

--- src/main/java/com/lk/service/CustomerService.java
+++ src/main/java/com/lk/service/CustomerService.java
@@ -1,17 +1,30 @@
 package com.lk.service;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lk.helper.DatabaseHelper;
 import com.lk.model.Customer;
+import com.lk.util.PropsUtil;
 
 public class CustomerService {
+    
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    
+    
     /**
      * 获取客户列表
      */
     public List<Customer> getCustomerList() {
         //
-        return null;
+        String sql = "select * from customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
     
     public Customer getCustomer(long id) {
